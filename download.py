d8b7a655aae7d97f8449ddc143fb81dac42b3f07
riccardop <riccardopirani@users.noreply.github.com>
2022-11-15 12:55:08 +0100

    Update download.py

--- download.py
+++ download.py
@@ -3,7 +3,7 @@ import shutil
 # Nome del cimitero
 nomecimitero = "cimitero2.zip"
 # Download file from url
-url = 'http://memoryp.org:3001/cimitero/get/'+nomecimitero
+url = 'http://url.org:3001/cimitero/get/'+nomecimitero
 r = requests.get(url, allow_redirects=True)
 open(nomecimitero, 'wb').write(r.content)
 # Unzipping file
